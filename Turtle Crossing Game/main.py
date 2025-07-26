c1aa4a5053b57376ddfb57bc349c7e38bd6215ab
Shikhar Vishwakarma <shikharvishwa123@gmail.com>
2025-07-26 23:10:28 +0530

    Create main.py

---
+++ Turtle Crossing Game/main.py
@@ -0,0 +1,44 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+
+cars = CarManager()
+
+
+scoreboard = Scoreboard()
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    cars.create_car()
+    cars.move_car()
+
+    for car in cars.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish():
+        player.goto_start()
+        cars.level_up()
+        scoreboard.level_up()
+
+
+screen.exitonclick()
